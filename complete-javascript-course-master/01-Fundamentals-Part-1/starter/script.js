7a13d2d9629ead31fee86c9d47dae52ddb3c9cfa
Vishvadeep Singh Chauhan <vishvadeepsinghchauhan@gmail.com>
2025-09-17 21:48:05 +0530

    Fundamental part1

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,22 @@
+let js = "amazing";
+console.log("hello")
+console.log(js);
+console.log(23)
+console.log(40+8+23-10);
+
+let firstName = "vishv";
+console.log(firstName);
+
+
+
+// Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+
+// Log their values to the console.
+
+
+let country = "India"
+let continent = "asia"
+let population = "2"
+
+console.log(`${country} is my country and continent is: ${continent} with population ${population}`);
+
