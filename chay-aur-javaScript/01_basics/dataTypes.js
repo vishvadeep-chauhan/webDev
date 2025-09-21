e69d4cefbd318aa3f783ebb76c269bd1040816fd
Vishvadeep Singh Chauhan <vishvadeepsinghchauhan@gmail.com>
2025-09-21 16:41:29 +0530

    dataTypes

---
+++ chay-aur-javaScript/01_basics/dataTypes.js
@@ -0,0 +1,39 @@
+// primitive 
+
+// Number , boolean , bigint , symbol , string , undefined , null 
+
+// ReferenceType ( non primitive)
+
+// Array , function , objects 
+
+const sup = ["hero1", "hero2" , "hero3"];
+
+const myObj = {
+    name: "vishv",
+    age: 22,
+    city: "Noida"
+
+}
+
+const myFunction = function(){
+    console.log("hello")
+}
+
+function myFun(){
+    console.log("hello")
+}
+
+
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
