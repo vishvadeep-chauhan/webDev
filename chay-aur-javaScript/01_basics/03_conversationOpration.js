a6b63d3cc1073d778ada8bef300b58461a1c412f
Vishvadeep Singh Chauhan <vishvadeepsinghchauhan@gmail.com>
2025-09-21 15:33:54 +0530

    Add examples for type conversion and comparison in JS
    Added two new files demonstrating JavaScript type conversion and comparison operations. '03_conversationOpration.js' shows how string values are converted to numbers and the resulting types, while '04_comparision.js' provides examples of comparison operators, including edge cases with null values.

---
+++ chay-aur-javaScript/01_basics/03_conversationOpration.js
@@ -0,0 +1,14 @@
+let score = "33abc" ;
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof(valueInNumber));
+
+console.log(valueInNumber);
+
+// "123". => 123
+// "123abc" => NAN
+// true => 1; , false => 0;
+
