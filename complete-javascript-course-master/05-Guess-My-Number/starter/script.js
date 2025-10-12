fea37cdabdbc82036101377c24f0bb0a80f4e4a7
Vishvadeep Singh Chauhan <vishvadeepsinghchauhan@gmail.com>
2025-10-12 17:19:15 +0530

    p

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,24 @@
-'use strict';
+console.log(document.querySelector('.message').textContent);
+
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+
+const x = function(){
+    console.log(23);
+}
+
+
+document.querySelector('.check').addEventListener('click', function(){
+  const guess = Number(document.querySelector('.guess').value);
+
+  console.log(guess, typeof guess);
+
+  if(!guess){
+    document.querySelector('.message').textContent = "⛔️ No Number";
+  }
+
+})
