f851aa240279110d0d435bdecb54c0833570431a
Vishvadeep Singh Chauhan <vishvadeepsinghchauhan@gmail.com>
2025-10-17 17:03:43 +0530

    read write file in nodejs

--- complete-node-bootcamp-master/1-node-farm/starter/index.js
+++ complete-node-bootcamp-master/1-node-farm/starter/index.js
@@ -1,4 +1,14 @@
-const hello = 'Hello World';
+const fs = require("fs");
 
-console.log(hello)
+const hello = "Hello World";
+
+console.log(hello);
+
+const textIn = fs.readFileSync( __dirname + "/txt/input.txt", "utf-8");
+console.log(textIn);
+
+const textOut = ` this is what we know about ${textIn}.\n created on ${Date.now()}`;
+fs.writeFileSync(__dirname + "/txt/output.txt", textOut);
+
+console.log('file written!')
 
