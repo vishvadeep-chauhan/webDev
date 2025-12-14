33a00704692c7c0ae5fa0c3e4fef99e08c2ac5a4
Vishvadeep Singh Chauhan <vishvadeepsinghchauhan@gmail.com>
2025-12-15 04:33:41 +0530

    feat: implement core game logic for Guess My Number, including input validation, score management, and win/lose conditions.

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -2,20 +2,78 @@
 
 // console.log(document.querySelector('.message').textContent);
 
-
 // document.querySelector('.message').textContent = 'correct Number';
-
 
 // document.querySelector('.number').textContent = 13 ;
 // document.querySelector('.score').textContent = 10;
 
 // document.querySelector('.guess').value = 23;
 
-
 // const btnCheck = document.querySelector('.check');
 
-document.querySelector('.check').addEventListener('click', function(){
-    console.log('Button Was clicked!');
-    
-})
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+
+  // when there is no input
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number';
+
+ // when player win
+
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+
+
+document.querySelector('.number').textContent = secretNumber; 
+
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+
+
+    // when guess too high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'too High ';
+
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'you lost the game ';
+      document.querySelector('.score').textContent = 0;
+    }
+
+    // when guess too low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'too Low ';
+
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'you lost the game ';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+
+
+// Coding Challenge #1
+/*
+Implement a game rest functionality, so that the player can make a new guess! Here is how:
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the score and secretNumber variables
+3. Restore the initial conditions of the message, number, score and guess input field
+4. Also restore the original background color (#222)
+and number width (15rem)
+GOOD LUCK */
 
