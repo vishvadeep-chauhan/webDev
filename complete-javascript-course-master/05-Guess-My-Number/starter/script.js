53ffc528b997e8edad97227fbff0d2802ccde692
Vishvadeep Singh Chauhan <vishvadeepsinghchauhan@gmail.com>
2025-12-20 12:29:56 +0530

    feat: refactor message display logic in Guess My Number game for improved readability

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -16,17 +16,24 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
 
+const displayMessage = function(message){
+  document.querySelector('.message').textContent = message;
+}
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   // when there is no input
   if (!guess) {
-    document.querySelector('.message').textContent = 'No Number';
+    // document.querySelector('.message').textContent = 'No Number';
+    displayMessage('No Number');
+
 
     // when player win
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'Correct Number!';
+    // document.querySelector('.message').textContent = 'Correct Number!';
+    displayMessage('Correct Number!');
 
     document.querySelector('.number').textContent = secretNumber;
 
@@ -41,13 +48,20 @@ document.querySelector('.check').addEventListener('click', function () {
     // when guess is wrong high
   } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent =
-        guess > secretNumber ? 'Too High' : 'Too Low';
+      // document.querySelector('.message').textContent =
+        // guess > secretNumber ? 'Too High' : 'Too Low';
+
+        displayMessage(guess > secretNumber ? 'Too High' : 'Too Low');
 
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = 'you lost the game ';
+      // document.querySelector('.message').textContent = 'you lost the game';
+
+      displayMessage('you lost the game');
+
+
+
       document.querySelector('.score').textContent = 0;
     }
   }
@@ -97,7 +111,11 @@ document.querySelector('.again').addEventListener('click', function () {
 
   document.querySelector('.score').textContent = score;
 
-  document.querySelector('.message').textContent = 'Start guessing...';
+  // document.querySelector('.message').textContent = 'Start guessing...';
+
+  displayMessage('Start guessing...');
+
+
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
 
