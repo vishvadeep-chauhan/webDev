a82d693bb204dd74be18da5e341c00b2594fe902
Vishvadeep Singh Chauhan <vishvadeepsinghchauhan@gmail.com>
2026-02-07 11:40:57 +0530

    feat: add new JavaScript functions for greeting, tea making, and calculations

---
+++ Udemy-Hitesh-webdev/04-javascript/10-js-foundation/53.js
@@ -0,0 +1,62 @@
+function greetUser(name){
+    return `Hello, ${name}`
+}
+
+let greetResult = greetUser("vishv")
+console.log(greetResult)
+
+function makeTea(teaType){
+    return `Making, ${teaType}`
+}
+
+let teaOrder = makeTea("lemon tea")
+
+console.log(teaOrder);
+
+// -----------------
+
+function addNumbers(a,b){
+return a + b;
+}
+
+let sumResult = addNumbers(20, 30)
+
+console.log(sumResult);
+
+function calculateTotal(price, quantity){
+
+    return price* quantity
+}
+
+let totalCost = calculateTotal(150,4)
+
+console.log(totalCost)
+
+
+// ----------------------------
+const calculateTotal2 = (price, quantity) => price* quantity;
+
+let updatedTotalCost = calculateTotal2(80,5)
+
+console.log(updatedTotalCost)
+
+// sec - 4
+
+function orderTea(teaType){
+    function confirmOrder(){
+        return `teaType`
+    }
+
+    return confirmOrder
+
+}
+
+let orderConfirmation = orderTea("chai")
+
+console.log(`Order confirmed for, ${orderConfirmation}`)
+
+
+
+function processTeaOrder(){
+    
+}
