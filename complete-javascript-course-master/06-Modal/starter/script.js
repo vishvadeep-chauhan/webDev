95f75320cc38e5f003a588cfc62988f44b87ca8b
Vishvadeep Singh Chauhan <vishvadeepsinghchauhan@gmail.com>
2026-02-11 14:12:58 +0530

    85. Handling an "Esc" Keypress Event

--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -6,6 +6,10 @@ const btnCloseModal = document.querySelector('.close-modal')
 const btnsOpenModal = document.querySelectorAll('.show-modal')
 
 console.log(btnsOpenModal);
+// --------------------------------------------
+// 84. working with classes 
+// ------------------------------------------------
+
 
 // for(let i = 0; i < btnsOpenModal.length; i++ ){
 //     btnsOpenModal[i].addEventListener('click', function(){
@@ -37,4 +41,27 @@ for(let i = 0; i < btnsOpenModal.length; i++){
     btnsOpenModal[i].addEventListener('click', openModal)
 }
 btnCloseModal.addEventListener('click', closeModal);
+
+
+
+// ------------------------------------
+// 85. Handling an "Esc" Keypress Event
+// ------------------------------------------------
+
+document.addEventListener('keydown', function(e) {
+    // console.log(e)
+    // console.log(e.key)
+
+//      if (e.key === 'Escape') {
+//     if (!modal.classList.contains('hidden')) {
+//       closeModal();
+//     }
+//   }
+
+
+
+    if(e.key === 'Escape' && !modal.classList.contains('hidden') ){
+            closeModal();
+    }
+});
 
