1baab925f8dc78ed58f1f927aa3ca2a745edd163
Vishvadeep Singh Chauhan <vishvadeepsinghchauhan@gmail.com>
2026-02-12 19:54:05 +0530

    88. Switching the Active Player

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const player0El = document.querySelector('.player--0')
+const player1El = document.querySelector('.player--1')
 
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
@@ -12,40 +14,41 @@ const btnHold = document.querySelector('.btn--hold');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
 
-
-
-
-
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-
-
-// OUTSIDE event listener (top of file)
+const scores = [0, 0];
 let currentScore = 0;
-
+let activePlayer = 0;
 
 btnRoll.addEventListener('click', function(){
     // 1. Generate random dice roll
 const dice = Math.trunc(Math.random() * 6) + 1;
-
+console.log(dice);
 
   // 2. Display dice
-diceEl.textContent.remove('hidden');
+diceEl.classList.remove('hidden');
 diceEl.src = `dice-${dice}.png`
 
   // 3. Check for rolled 1: if true, switch to next player
 
 if(dice !== 1){
+    // add dice to current score 
+
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
 
 }else {
     // Switch to next player
 
+    document.getElementById(`current--${activePlayer}`).textContent = 0
+     currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0
+    player0El.classList.toggle('player--active')
+    player1El.classList.toggle('player--active')
+
 
 }
-
 
 })
