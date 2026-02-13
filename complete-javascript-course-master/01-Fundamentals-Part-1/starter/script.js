30998e04cdf257dd0c8a5fdad7f0731e504d7da4
Vishvadeep Singh Chauhan <vishvadeepsinghchauhan@gmail.com>
2026-02-13 22:07:59 +0530

    Refactor population logging and description variables

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -8,7 +8,6 @@ let firstName = "vishv";
 console.log(firstName);
 
 
-
 // Declare variables called country, continent and population and assign their values according to your own country (population in millions).
 
 // Log their values to the console.
@@ -20,3 +19,33 @@ let population = "2"
 
 console.log(`${country} is my country and continent is: ${continent} with population ${population}`);
 
+
+console.log(population / 2);
+
+population++;
+
+console.log(population);
+console.log(population > 6);
+console.log(population < 33);
+
+
+
+
+
+const description1 =
+  country +
+  ' is in ' +
+  continent +
+  ', and its ' +
+  population +
+  ' million people speak ' +
+  language;
+
+console.log(description1);
+
+
+
+
+
+
+const description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
