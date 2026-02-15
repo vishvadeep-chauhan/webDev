c107c76f75f4b6f0d1de8df3b6002adfc2463c91
Vishvadeep Singh Chauhan <vishvadeepsinghchauhan@gmail.com>
2026-02-15 18:34:13 +0530

    Add initial implementation of BMI calculation for Mark and John

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/ex/excrise.js
@@ -0,0 +1,20 @@
+/* Write your code below. Good luck! 🙂 */
+
+let massMark = 78
+let heightMark = 1.69
+
+let massJohn = 92
+let heightJohn = 1.95
+
+let BMIMark = massMark / (heightMark * heightMark)
+let BMIJohn = massMark / (heightMark * heightMark)
+
+
+console.log(BMIMark);
+console.log(BMIJohn);
+
+let markHigherBMI = BMIMark >  BMIJohn;
+console.log(markHigherBMI);
+
+
+
