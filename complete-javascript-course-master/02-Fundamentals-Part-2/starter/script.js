6d44371d1493193c6affb0a95bec0e435894f8ab
Vishvadeep Singh Chauhan <vishvadeepsinghchauhan@gmail.com>
2026-02-21 14:50:39 +0530

    Comment out unused variables and functions in script.js for clarity

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,33 +1,56 @@
-const friend1 = 'Michael';
-const friend2 = 'Steven';
+// const friend1 = 'Michael';
+// const friend2 = 'Steven';
 
-const friend = ['Michael', 'Steven', 'Peter'];
-console.log(friend);
+// const friend = ['Michael', 'Steven', 'Peter'];
+// console.log(friend);
 
-const year = new Array(1991, 1998, 1997, 1996);
+// const year = new Array(1991, 1998, 1997, 1996);
 
-console.log(year)
-console.log(friend1[0])
-console.log(friend2[0])
+// console.log(year)
+// console.log(friend1[0])
+// console.log(friend2[0])
 
-console.log(friend.length)
+// console.log(friend.length)
 
-console.log(friend[friend.length-1])
+// console.log(friend[friend.length-1])
 
-const jonas = ['jonas', 'sandman', 4303 - 2323 , 'teacher' , friend]
+// const jonas = ['jonas', 'sandman', 4303 - 2323 , 'teacher' , friend]
 
-console.log(jonas)
+// console.log(jonas)
 
-console.log(jonas.length)
+// console.log(jonas.length)
 
-const calcAge = function (birthYear){
-  return 2030 - birthYear
-}
+// const calcAge = function (birthYear){
+//   return 2030 - birthYear
+// }
 
-const years = [1990, 1999, 1998 , 19997 ,  1996]
+// const years = [1990, 1999, 1998 , 19997 ,  1996]
 
-const age1 = calcAge(year[0]);
-const age2 = calcAge(year[1]);
-const age3 = calcAge(year[year.length - 1]);
-console.log(age1, age2, age3);
+// const age1 = calcAge(year[0]);
+// const age2 = calcAge(year[1]);
+// const age3 = calcAge(year[year.length - 1]);
+// console.log(age1, age2, age3);
+
+const friends = ['michel', 'steven',  'peter'];
+const newLength =  friends.push('jay')
+console.log(friends)
+console.log(newLength);
+
+friends.unshift('john')
+console.log(friends)
+
+friends.pop();
+const popped = friends.pop()
+console.log(popped)
+console.log(friends)
+
+
+friends.shift()
+console.log(friends)
+
+
+console.log(friends.indexOf('bob'))
+
+
+
 
