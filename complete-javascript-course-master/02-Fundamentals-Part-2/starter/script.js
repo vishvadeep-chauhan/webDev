08bb0b4c92b38b58d493eca7e246c7129d4124f8
Vishvadeep Singh Chauhan <vishvadeepsinghchauhan@gmail.com>
2026-02-21 16:28:17 +0530

    Comment out unused tip calculation code and related variables for clarity

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -54,16 +54,38 @@
 
 
 
-let calcTip = (bill) => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20 ;
-
-console.log(calcTip(100))
-
-const bills = [125, 555, 44]
-
-const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
-
-const totals = [bills[0] + tips[0] , bills[1] + tips[1], bills[2]+ tips[2]]
 
 
-console.log(bills, tips , totals)
+// let calcTip = (bill) => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20 ;
 
+// console.log(calcTip(100))
+
+// const bills = [125, 555, 44]
+
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
+
+// const totals = [bills[0] + tips[0] , bills[1] + tips[1], bills[2]+ tips[2]]
+
+
+// console.log(bills, tips , totals)
+
+
+// const jonas = {
+//   firstName: 'jonas',
+//   lastName: 'schmedtmann',
+//   age: 2037 - 1991,
+//   job: 'teacher',
+//   friends: ['micheal', 'peter', 'Steven']
+// };
+
+
+const person = {
+  firstName: "Vishv",
+  lastName: "Deep", 
+  birthYear: 1998,
+  job: "student",
+  friends: ["Aman", "Rahul", "Neeraj"]
+
+}
+
+console.log(`${person.firstName} has ${person.friends.length} friends, and his best friend is ${person.friends[0]}` )
