192f8ac76546602e7b1dc15bb9982f010ede47a4
Vishvadeep Singh Chauhan <vishvadeepsinghchauhan@gmail.com>
2026-03-16 16:12:56 +0530

    Refactor restaurant object and add new methods for order handling

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -1,21 +1,4 @@
 
-// const restaurant = {
-//   name: 'Classico Italiano',
-//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
-//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
-//   // ES6 enhanced object literals
-
-//   order:  function(starterIndex, mainIndex){
-//     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
-
-//   }
-  
-
-
-// }
 
 
 // // Array Destructuring
@@ -370,19 +353,103 @@
 // ---------------------------------------------------------
 
 
-const product = {
-  title: "Laptop",
-  price: 50000,
-  tags: ["electronics", "computer"]
+// const product = {
+//   title: "Laptop",
+//   price: 50000,
+//   tags: ["electronics", "computer"]
+// };
+
+// const printProduct = ({title , price, tags:[firstTag , secondTag]}) =>{
+
+//   return `${title} costs ${price} and belongs to ${firstTag} category`
+// }
+
+// console.log(printProduct(product))
+
+// ---------------------------------------------------------
+// ---------------------------------------------------------
+
+
+'use strict';
+
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
 };
 
-const printProduct = ({title , price, tags:[firstTag , secondTag]}) =>{
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  return `${title} costs ${price} and belongs to ${firstTag} category`
-}
+  // ES6 enhanced object literals
+  openingHours,
 
-console.log(printProduct(product))
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 
-// ---------------------------------------------------------
-// ---------------------------------------------------------
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
+  orderPasta(ing1, ing2, ing3) {
+    console.log(
+      `Here is your declicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
+
+  orderPizza(mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
+};
+
+
+const arr = [7,8,9];
+
+const badNewArr = [1,2,arr[0], arr[1], arr[2]]
+console.log(badNewArr)
+
+const newArr = [1,2, ...arr];
+
+console.log(newArr)
+
+console.log(...newArr)
+
+const newManu = [...restaurant.mainMenu, 'Gnocci']
+console.log(newManu);
+
+// copyArray
+
+
+const mainMenuCopy = [...restaurant.mainMenu];
+
+const menu = [ ...restaurant.starterMenu, ...restaurant.mainMenu ]
+
+console.log(menu)
+
+const str = 'jonas'
+
+const letter = [...str, ' ' , 's.']
+
+console.log(letter)
+console.log(...str);
+
+// console.log(`${...str} `)
 
